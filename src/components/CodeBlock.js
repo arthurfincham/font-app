b85d1dd565f7df2ca2a6ade6da6dbfd708058624
Arthur Fincham <arthurfincham@me.com>
2022-01-02 21:00:38 +0000

    extract code snippet from code block component

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -4,45 +4,24 @@ import Tooltip from '@mui/material/Tooltip';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { grayscale } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import CodeSnippet from './CodeSnippet';
 
 export default function CodeBlock({ font, fontWeight, htmlTag, transform, fontItalic }) {
   const transForm = `\ntransform: '${transform}';`;
   const italicStyle = `\nfont-style: 'italic';`;
   const cssBlock = `font-family: '${font}';\nfont-weight: '${fontWeight}';${transform ? transForm : ''}${fontItalic ? italicStyle : ''}`;
-
   const [copiedHTML, setCopiedHTML] = useState(false);
-
   const [copiedCSS, setCopiedCSS] = useState(false);
-
   const [value, setValue] = useState(cssBlock);
-
   const finalHTMLTag = htmlTag(font.replace(/\s/g, '+'));
-
-  const entireHTMLTAG =
+  const entireHTMLTag =
     `<link rel='preconnect' href='https://fonts.googleapis.com' />
 <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />` + finalHTMLTag;
 
   return (
     <TableCell align="right" sx={{ width: '400px' }}>
-      <CopyToClipboard text={entireHTMLTAG}>
-        <Tooltip title={copiedHTML ? 'Copied!' : 'HTML'} placement="left">
-          <div>
-            <SyntaxHighlighter language="javascript" style={grayscale} wrapLongLines={true} onClick={() => setCopiedHTML(true)}>
-              {finalHTMLTag}
-            </SyntaxHighlighter>
-          </div>
-        </Tooltip>
-      </CopyToClipboard>
-
-      <CopyToClipboard text={value}>
-        <Tooltip title={copiedCSS ? 'Copied!' : 'CSS'} placement="left">
-          <div>
-            <SyntaxHighlighter language="css" style={grayscale} wrapLongLines={true} onClick={() => setCopiedCSS(true)}>
-              {cssBlock}
-            </SyntaxHighlighter>
-          </div>
-        </Tooltip>
-      </CopyToClipboard>
+      <CodeSnippet language="HTML" preview={finalHTMLTag} content={entireHTMLTag} />
+      <CodeSnippet language="CSS" preview={cssBlock} content={cssBlock} />
     </TableCell>
   );
 }
---
+++ src/components/CodeSnippet.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import Tooltip from '@mui/material/Tooltip';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { grayscale } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+export default function CodeSnippet({ language, preview, content }) {
+  const [copied, setCopied] = useState(false);
+
+  return (
+    <CopyToClipboard text={content}>
+      <Tooltip title={copied ? 'Copied!' : language} placement="left">
+        <div>
+          <SyntaxHighlighter language={language} style={grayscale} wrapLongLines={true} onClick={() => setCopied(true)}>
+            {preview}
+          </SyntaxHighlighter>
+        </div>
+      </Tooltip>
+    </CopyToClipboard>
+  );
+}
