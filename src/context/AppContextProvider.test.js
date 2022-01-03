4ba3a2485ffafe36d54b9862f940530a8d882687
Arthur Fincham <arthurfincham@me.com>
2022-01-03 15:43:28 +0000

    finish AppContextProvider test

---
+++ src/context/AppContextProvider.test.js
@@ -0,0 +1,12 @@
+import AppContextProvider from './AppContextProvider';
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+Enzyme.configure({ adapter: new Adapter() });
+
+it('combines other context providers', () => {
+  const wrapper = shallow(<AppContextProvider />);
+
+  expect(toJson(wrapper)).toMatchSnapshot();
+});
